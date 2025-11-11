69f76ea3221018d2f48203f40ce069e748e3635c
Tojan-Naiem <tojan050@gmail.com>
2025-11-11 23:05:06 +0200

    feat : add product controller

--- E-Commerce.API/Controllers/ProductsController.cs
+++ E-Commerce.API/Controllers/ProductsController.cs
@@ -1,11 +1,74 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using E_Commerce.BLL.Service.Classes;
+using E_Commerce.BLL.Service.Interfaces;
+using E_Commerce.DAL.DTO.Request;
+using E_Commerce.DAL.DTO.Response;
+using E_Commerce.DTO.Response;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace E_Commerce.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize(Roles = "Admin")]
+
     public class ProductsController : ControllerBase
     {
-    }
+   
+            private readonly IProductService _productService;
+
+            public ProductsController(
+              IProductService productService
+              )
+            {
+                _productService =  productService
+
+
+        }
+            [HttpGet]
+            [AllowAnonymous]
+            public async Task<ActionResult<List<ProductResponse>>> GetAll([FromQuery] string lang = "en")
+            {
+                var productsDto = _productService.GetAll(lang);
+                return Ok(new { productsDto });
+
+            }
+            [HttpGet("{id}")]
+            [AllowAnonymous]
+
+            public IActionResult Get([FromRoute] long id)
+            {
+            ProductResponse productResponse = _productService.GetById(id);
+                if (productResponse is null) return NotFound();
+                return Ok(productResponse);
+            }
+            [HttpPost]
+            public async Task<IActionResult> Create([FromBody] ProductRequest productRequest)
+            {
+                await _productService.Create(productRequest);
+                return StatusCode(StatusCodes.Status201Created);
+            }
+            [HttpPatch("{id}")]
+            public async Task<IActionResult> Update([FromRoute] long id, [FromBody] ProductRequest productRequest)
+            {
+                bool isExist = await _productService.Update(id, productRequest);
+                if (isExist is false) return NotFound(new { message = "Not found" });
+                return Ok(new { message = "Updated" });
+            }
+            [HttpPatch("{id}/toggle-status")]
+            public async Task<IActionResult> ToggleStatus([FromRoute] long id)
+            {
+                bool isExist = await _productService.ToggleStatus(id);
+                if (isExist is false) return NotFound(new { message = "Not found" });
+                return Ok(new { message = "Updated" });
+            }
+            [HttpDelete("{id}")]
+            public async Task<IActionResult> Delete([FromRoute] long id)
+            {
+                bool isExist = await _productService.Delete(id);
+                if (isExist is false) return NotFound(new { message = "Not found" });
+                return Ok(new { message = "Deleted" });
+            }
+        }
 }
