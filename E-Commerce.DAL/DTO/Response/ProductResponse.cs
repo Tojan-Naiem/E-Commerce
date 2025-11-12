2ec222e9a22e5924cf6ffeb14f8a754e9b829394
Tojan-Naiem <tojan050@gmail.com>
2025-11-12 11:24:17 +0200

    feat : add jsonIgnore for mainImage at ProductResponse

--- E-Commerce.DAL/DTO/Response/ProductResponse.cs
+++ E-Commerce.DAL/DTO/Response/ProductResponse.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using static System.Net.WebRequestMethods;
 
@@ -12,6 +13,7 @@ namespace E_Commerce.DAL.DTO.Response
     {
         public string Name { get; set; }
         public string Description { get; set; }
+        [JsonIgnore]
         public string MainImage { get; set; }
         public string MainImageUrl => $"https://localhost:7039/Images/{MainImage}";
 
