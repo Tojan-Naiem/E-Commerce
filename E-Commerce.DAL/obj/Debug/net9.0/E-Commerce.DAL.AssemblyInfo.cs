09a7a3e144a68667bb4cd73e693bc88ff7e0024c
Tojan-Naiem <tojan050@gmail.com>
2025-11-22 23:57:20 +0200

    feat : add check out response and request

--- E-Commerce.DAL/obj/Debug/net9.0/E-Commerce.DAL.AssemblyInfo.cs
+++ E-Commerce.DAL/obj/Debug/net9.0/E-Commerce.DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-Commerce.DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4da7c61dbe4cd335dc50197ad08af36857dfa64f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a737134d010598ebe6fe4ed9d768bdcfd204eaf")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-Commerce.DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-Commerce.DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
