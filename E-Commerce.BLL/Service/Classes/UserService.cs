b547288ec0739c0f0b7432a9d7d98ce693561a77
Tojan-Naiem <tojan050@gmail.com>
2025-12-19 20:07:00 +0200

    feat : add UnBlockUserAsync into service

--- E-Commerce.BLL/Service/Classes/UserService.cs
+++ E-Commerce.BLL/Service/Classes/UserService.cs
@@ -34,5 +34,10 @@ namespace E_Commerce.BLL.Service.Classes
             var result = await _userRepository.BlockUserAsync(UserId, days);
             return result;
         }
+        public async Task<bool> UnBlockUserAsync(string UserId, int days)
+        {
+            var result = await _userRepository.UnBlockUserAsync(UserId);
+            return result;
+        }
     }
 }
