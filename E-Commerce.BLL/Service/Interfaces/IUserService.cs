92ceb2cd73c3741d349baa27967e07c91e4dcc34
Tojan-Naiem <tojan050@gmail.com>
2025-12-19 19:13:58 +0200

    feat : add user service with interface

---
+++ E-Commerce.BLL/Service/Interfaces/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using E_Commerce.DAL.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.BLL.Service.Interfaces
+{
+    public interface IUserService
+    {
+        public Task<List<ApplicationUser>> GetAllAsync();
+
+    }
+}
