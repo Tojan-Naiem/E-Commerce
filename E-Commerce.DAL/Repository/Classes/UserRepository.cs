bab0a118cdd9f9f4bea83cf49497447be6b9dfc0
Tojan-Naiem <tojan050@gmail.com>
2025-12-19 19:15:53 +0200

    feat : add user repostiroy

---
+++ E-Commerce.DAL/Repository/Classes/UserRepository.cs
@@ -0,0 +1,13 @@
+﻿using E_Commerce.DAL.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.DAL.Repository.Classes
+{
+    public class UserRepository:IUserRepository
+    {
+    }
+}
