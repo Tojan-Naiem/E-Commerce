d33b14ffd4e6449fe4a71a97817ce2ec20e16b32
Tojan-Naiem <tojan050@gmail.com>
2025-12-19 19:34:28 +0200

    feat : add user dto for response instead of UserApplication

---
+++ E-Commerce.DAL/DTO/Response/UserDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.DAL.DTO.Response
+{
+    public class UserDTO
+    {
+        public string Id { get; set; }
+        public string FullName { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public bool EmailConfirmed { get; set; }
+    }
+}
