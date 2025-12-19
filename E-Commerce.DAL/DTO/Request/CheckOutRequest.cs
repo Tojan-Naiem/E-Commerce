50529d68423e3ae0f2f053ebb22e78e03448230d
Tojan-Naiem <tojan050@gmail.com>
2025-12-19 12:39:23 +0200

    feat : add JsonStringEnumConverter

--- E-Commerce.DAL/DTO/Request/CheckOutRequest.cs
+++ E-Commerce.DAL/DTO/Request/CheckOutRequest.cs
@@ -1,14 +1,17 @@
 ﻿using E_Commerce.DAL.Model;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace E_Commerce.DAL.DTO.Request
 {
     public class CheckOutRequest
     {
+        [JsonConverter(typeof(JsonStringEnumConverter))]
         public PaymentMethod PaymentMethod { get; set; }
     }
 }
