a3dabc8f9fc4d706c22fed750a543f33898b9803
Tojan-Naiem <tojan050@gmail.com>
2025-12-19 19:21:33 +0200

    feat : add getAllUsers and getUserById methods into user repository

--- E-Commerce.DAL/Repository/Interfaces/IUserRepository.cs
+++ E-Commerce.DAL/Repository/Interfaces/IUserRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using E_Commerce.DAL.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,7 @@ namespace E_Commerce.DAL.Repository.Interfaces
 {
     public interface IUserRepository
     {
+        public Task<List<ApplicationUser>> GetAllAsync();
+        public Task<ApplicationUser> GetByIdAsync(string UserId);
     }
 }
